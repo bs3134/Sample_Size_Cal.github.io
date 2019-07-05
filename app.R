6e5e4bfb3c8297faddbabbf25dd562ac2ed449ed
bihui sun <bihui2011@outlook.com>
2019-07-05 16:43:15 +0800

    complete app2, calculate two sample continuous data sample size

--- app.R
+++ app.R
@@ -1,22 +1,25 @@
-
+# Prop.test
 
 rm(list = ls())
 library(shiny)
 ui = shinyUI(fluidPage(
-    mainPanel(
+  mainPanel(
     numericInput("p0", "p0:",0.5, min = 0, max = 1),
     numericInput("p1", "p1:", 0.5,min = 0, max = 1),
     numericInput("alpha", "significant level:", 0.05,min = 0, max = 1),
     numericInput("power", "power:", 0.8,min = 0, max = 1),
+    checkboxInput('check', label = 'if check then use proportion test', value = FALSE)
+  ),
     textOutput("text_calc"))
-    ))
+)
 server = shinyServer(function(input, output,session){
-  output$text_calc <- renderText({
+  output$text_calc = renderText({
     p0 = input$p0
     p1 = input$p1
     alpha = input$alpha
     power = input$power
-    paste("The result is =", ((qnorm(1-alpha)*sqrt(p0*(1-p0))+qnorm(power)*sqrt(p1*(1-p1)))/(p1-p0))^2)
+    paste("The result is =", if (input$check){((qnorm(1-alpha)*sqrt(p0*(1-p0))+qnorm(power)*sqrt(p1*(1-p1)))/(p1-p0))^2}
+     else{0} )    
   })
 })
 
