c10c64ab5aa9c97ebfec14d267cb2c69e8b3fe65
bihui sun <bihui2011@outlook.com>
2019-07-11 09:30:29 +0800

    change three

--- app3/app.R
+++ app3/app.R
@@ -8,18 +8,22 @@ ui = shinyUI(fluidPage(
     numericInput("p1", "p1:", 0.5,min = 0, max = 1),
     numericInput("alpha", "significant level:", 0.05,min = 0, max = 1),
     numericInput("power", "power:", 0.8,min = 0, max = 1),
-    checkboxInput('check', label = 'if check then use one-sided test; if not, then use two-sided test', value = FALSE)
-  ),
-  textOutput("text_calc"))
-)
+    numericInput("delta","delta:",0.5),
+    selectInput("choice", label = h3("Select test type"),choices = c("equality","equivalence","inferiority/superiority")
+    ),
+    textOutput("text_calc"))
+))
 server = shinyServer(function(input, output,session){
   output$text_calc = renderText({
     p0 = input$p0
     p1 = input$p1
     alpha = input$alpha
     power = input$power
-    paste("The result is =", if (input$check){(2*((qnorm(alpha)+qnorm(1-power))^2)*((p0+p1)/2)*(1-(p0+p1)/2))/(p0-p1)^2}
-          else{(2*((qnorm(alpha/2)+qnorm(1-power))^2)*((p0+p1)/2)*(1-(p0+p1)/2))/(p0-p1)^2} )    
+    delta = input$delta
+    choice = input$choice
+    paste("The result is =", if (choice=="equality"){(2*((qnorm(alpha)+qnorm(1-power))^2)*((p0+p1)/2)*(1-(p0+p1)/2))/(p0-p1)^2}
+          else if (choice=="equivalence"){(((qnorm(alpha)+qnorm((1-power)/2))^2)*(p0*(1-p0)+p1*(1-p1)))/(delta-abs(p0-p1))^2}
+          else {(((qnorm(alpha)+qnorm(1-power))^2)*(p0*(1-p0)+p1*(1-p1)))/(delta-(p0-p1))^2})    
   })
 })
 
